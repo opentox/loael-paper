07d6766d49b6a07ddde69788c4bf614be899651f
gebele <gebele@in-silico.ch>
2015-09-01 12:46:14 +0000

    NOAL removed; endpoint description added

--- create_nestec_models.rb
+++ create_nestec_models.rb
@@ -3,7 +3,7 @@ include OpenTox
 $mongo.database.drop
 $gridfs = $mongo.database.fs # recreate GridFS indexes
 
-["Rat_TD50.csv","Mouse_TD50.csv","LOAEL_mmol_corrected_smiles.csv"].each do |file|
+["Rat_TD50.csv","Mouse_TD50.csv"].each do |file|
   file = File.join(File.dirname(__FILE__),"regression",file)
   Model::Prediction.from_csv_file file
 end
