35e4ac2fbe4c76c599715d264f0fdb9ef7857173
Christoph Helma <helma@in-silico.ch>
2015-10-08 10:29:18 +0200

    updated models with repeated crossvalidation

--- create_nestec_models.rb
+++ create_nestec_models.rb
@@ -1,10 +1,14 @@
 require_relative '../lazar/lib/lazar'
 include OpenTox
-$mongo.database.drop
-$gridfs = $mongo.database.fs # recreate GridFS indexes
+#$mongo.database.drop
+#$gridfs = $mongo.database.fs # recreate GridFS indexes
 
-["Rat_TD50.csv","Mouse_TD50.csv"].each do |file|
+[
+  #"Rat_TD50.csv",
+  #"Mouse_TD50.csv",
+  "LOAEL_mmol_corrected_smiles.csv"
+].each do |file|
   file = File.join(File.dirname(__FILE__),"regression",file)
   Model::Prediction.from_csv_file file
 end
-#`mongodump`
+`mongodump -h 127.0.0.1 -d opentox`
