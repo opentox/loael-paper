3a05be7bec574ac6b9e037509a702f2446de14db
gebele <gebele@in-silico.ch>
2015-10-13 09:11:57 +0000

    updated LOAEL metadata

--- create_nestec_models.rb
+++ create_nestec_models.rb
@@ -2,13 +2,15 @@ require_relative '../lazar/lib/lazar'
 include OpenTox
 #$mongo.database.drop
 #$gridfs = $mongo.database.fs # recreate GridFS indexes
-
+#=begin
 [
-  #"Rat_TD50.csv",
-  #"Mouse_TD50.csv",
+  "Rat_TD50.csv",
+  "Mouse_TD50.csv",
   "LOAEL_mmol_corrected_smiles.csv"
 ].each do |file|
   file = File.join(File.dirname(__FILE__),"regression",file)
   Model::Prediction.from_csv_file file
 end
-`mongodump -h 127.0.0.1 -d opentox`
+#=end
+`mongodump -h 127.0.0.1 -d production`
+#`mongorestore --host 127.0.0.1`
